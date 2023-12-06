2eccb2014ce607c309d56ea5467d30445a068e59
QMStar <qmstar0@qq.com>
2023-12-06 13:33:56 +0800

    added dnd

---
+++ src/components/dnd/dnd.jsx
@@ -0,0 +1,44 @@
+import {DragDropSensors, DragDropProvider, createDraggable, createDroppable, useDragDropContext} from "@thisbeyond/solid-dnd";
+
+function DragEvent(props) {
+    const [, {onDragEnd}] = useDragDropContext();
+    onDragEnd(({draggable, droppable}) => {
+        console.log(draggable, droppable)
+        if (droppable) {
+            if (droppable.id === 'selected-tags' && droppable.node.children.length >= 4) {
+                return
+            }
+            droppable.node.appendChild(draggable.node)
+        }
+    });
+    return props.children
+}
+
+export function Dnd(props) {
+    return (
+        <div class={props.class}>
+            <DragDropProvider>
+                <DragDropSensors>
+                    <DragEvent>
+                        {props.children}
+                    </DragEvent>
+                </DragDropSensors>
+            </DragDropProvider>
+        </div>
+    )
+}
+
+
+export function DragBox(props) {
+    const draggable = createDraggable(props.id);
+    return (<>
+        <div use:draggable class={props.class}>{props.children}</div>
+    </>)
+}
+
+export function DropBox(props) {
+    const droppable = createDroppable(props.id);
+    return (<>
+        <div use:droppable class={props.class}>{props.children}</div>
+    </>)
+}
