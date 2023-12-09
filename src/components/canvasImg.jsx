4b67f588ca90d267a3699fc144cd0c12ecc36b56
QMStar <qmstar0@qq.com>
2023-12-09 17:39:50 +0800

    Complete profile

--- src/components/canvasImg.jsx
+++ src/components/canvasImg.jsx
@@ -1,31 +1,35 @@
 import {onMount} from "solid-js";
 
-export default function CanvasImg({src}) {
+export default function CanvasImg(props) {
     let canvas;
-    const w = 400, h = 400;
+    const height = props.height,
+        width = props.width;
     onMount(() => {
         const ctx = canvas.getContext("2d");
         const img = new Image();
         ctx.imageSmoothingEnabled = !1
-        img.src = src;
+        img.src = props.src;
+
+        const gradient = ctx.createLinearGradient(0, height * 0.75, 0, height); // 创建一个线性渐变对象
+        gradient.addColorStop(0, "rgba(255, 255, 255, 0.5)")
+        gradient.addColorStop(0.3, "rgba(255, 255, 255, 0.7)")
+        gradient.addColorStop(0.5, "rgba(255, 255, 255, 0.85)")
+        gradient.addColorStop(0.7, "rgba(255, 255, 255, 1)")
 
         img.onload = function () {
-            ctx.drawImage(img, 0, 0, img.width, img.height, w / 4, h / 4, w / 2, h / 2)
+            ctx.drawImage(img, 0, 0, img.width, img.height, width / 4, height / 4, width / 2, height / 2)
             ctx.save(); // 保存当前状态
             ctx.scale(1, -1); // 垂直翻转
-            ctx.drawImage(img, 0, 0, img.width, img.height, w / 4, -(h * 1.25), w / 2, h / 2)
+            ctx.drawImage(img, 0, 0, img.width, img.height, width / 4, -(height * 1.25), width / 2, height / 2)
             ctx.restore(); // 恢复之前的状态
             ctx.globalCompositeOperation = "destination-out"; // 设置合成模式为 destination-out
-            const gradient = ctx.createLinearGradient(0, h * 0.75, 0, h); // 创建一个线性渐变对象
-            gradient.addColorStop(0, "rgba(255, 255, 255, 0.5)")
-            gradient.addColorStop(0.3, "rgba(255, 255, 255, 0.7)")
-            gradient.addColorStop(0.5, "rgba(255, 255, 255, 0.85)")
-            gradient.addColorStop(0.7, "rgba(255, 255, 255, 1)")
             ctx.fillStyle = gradient; // 设置填充样式为渐变对象
-            ctx.fillRect(0, h * 0.75, canvas.width, h * 0.75); // 绘制一个填充的矩形
+            ctx.fillRect(0, height * 0.75, canvas.width, height * 0.75); // 绘制一个填充的矩形
         }
     })
     return (<>
-        <canvas class="w-[240px] h-[240px]" width={w} height={h} ref={canvas}/>
+        <div class="w-fit mx-auto -mb-[40px]">
+            <canvas {...props} ref={canvas}/>
+        </div>
     </>)
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,22 @@ module.exports = {
             fontFamily: {
                 code: ['FiraCode'],
                 lxgw: ['LXGW'],
-            }, colors: {
+            },
+            colors: {
                 tdc: 'var(--theme-default-color)', bdc: 'var(--backend-default-color)', bdcs: 'var(--backend-default-color-shadow)'
+            },
+            keyframes: {
+                rain: {
+                    '0%': {transform: 'translateY(0) scaleY(1)'},
+                    '70%': {transform: 'translateY(180px) scaleY(1)'},
+                    '80%': {transform: 'translateY(180px) scaleY(0.2)'},
+                    '100%': {transform: 'translateY(180px) scaleY(0.2) scaleX(15)'},
+                }
+            },
+            animation: {
+                rain: 'rain 0.8s linear infinite'
             }
+
         }
     }, plugins: [{
         'tailwindcss/nesting': require('postcss-nesting'),
