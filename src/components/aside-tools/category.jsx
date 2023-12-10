d39452de9dded7b2c57d9483f3b3e246d355ccd3
QMStar <qmstar0@qq.com>
2023-12-10 19:25:18 +0800

    Complete aside

--- src/components/aside-tools/category.jsx
+++ src/components/aside-tools/category.jsx
@@ -1,20 +1,21 @@
-import {For} from "solid-js";
+import {For, Show} from "solid-js";
 import {A} from "solid-start";
 
 export default function AsideCategory({data}) {
     return (
-        <>
+        <><Show when={data}>
             <div class="mb-8">
                 <h3 class="select-none text-[20px] mb-4 text-gray-600">类别</h3>
                 <ul class="font-lxgw ml-8 text-tdc2">
                     <For each={data}>{(item) => <>
-                        <li class="my-1 relative before:content-['-'] before:absolute before:-left-[18px]">
+                        <li class="my-1 relative before:content-['-'] before:absolute before:text-gray-600 before:-left-[18px]">
                             <A class="hover:underline" href="/">{item}</A>
                         </li>
                     </>}
                     </For>
                 </ul>
             </div>
+        </Show>
         </>
     )
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import Header from "~/components//header/Header";
 import HeroSections from "~/components/Hero-Sections.jsx";
-import {For} from "solid-js";
+import {For, onCleanup, onMount} from "solid-js";
 import ArticleItem from "~/components/article/article-item.jsx";
 import ArticleImgItem from "~/components/article/article-img-item.jsx";
 import Profile from "~/components/profile.jsx";
@@ -11,18 +11,29 @@ import AsideFooter from "~/components/aside-tools/footer.jsx";
 
 
 export default function Index() {
-
+    let aside;
+    onMount(() => {
+        const setAside = () => {
+            const top = window.innerHeight - aside.clientHeight
+            aside.style.top = `${top}px`
+        }
+        setAside()
+        window.addEventListener("resize", setAside)
+        onCleanup(() => {
+            window.removeEventListener("resize", setAside)
+        })
+    })
     return (<>
         <Header/>
         <HeroSections/>
-        <section class="flex flex-row mx-auto py-12 max-w-5xl">
+        <section class="flex flex-row mx-auto pt-12 max-w-5xl">
             <main class="font-lxgw flex-1 mr-8 pb-8">
                 <For each={[1, 2]}>{(item) => (<>
                     <ArticleItem/>
                     <ArticleImgItem/>
                 </>)}</For>
             </main>
-            <aside class="w-[280px] h-fit">
+            <aside ref={aside} class="w-[280px] h-fit px-4 sticky">
                 <Profile/>
                 <AsideRecommend data={['test']}/>
                 <AsideCategory data={['test']}/>
